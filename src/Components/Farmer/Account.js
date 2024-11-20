90d1ca7fbdc8836c9b2a02862aaf395e1c075d8e
AkkuAku <akku.saniyazbekova@nu.edu.kz>
2024-11-20 21:15:46 +0600

    Update Account.js

--- src/Components/Farmer/Account.js
+++ src/Components/Farmer/Account.js
@@ -126,6 +126,11 @@ const Account = () => {
           className="main-logo"
         />
         <div className="right-section">
+            <img
+            src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
+            alt="Mail Icon"
+            className="mail-icon"
+          />
           <button
             className="account-button"
             onClick={() => navigate("/account")}
